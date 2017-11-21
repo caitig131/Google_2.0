228da1da9096a8ebfaebc0db750df73a4eaef90f
caitig131 <31958666+caitig131@users.noreply.github.com>
2017-11-21 11:35:28 -0500

    Update Google2.0.py

--- Google2.0.py
+++ Google2.0.py
@@ -3,8 +3,6 @@ if question == "Why is the sky blue?":
     print("Because")
 if question == "What is the meaning of life?":
     print("To be loved and to love others")
-if question == "Who is God?":
-    print("Something people made up")
 if question == "What is the funniest joke?":
     print("Your face")
 else:
