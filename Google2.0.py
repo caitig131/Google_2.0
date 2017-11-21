fc5f7398408c081f090077790290bbdca2cf579e
caitig131 <31958666+caitig131@users.noreply.github.com>
2017-11-21 11:35:03 -0500

    Update Google2.0.py

---
+++ Google2.0.py
@@ -0,0 +1,11 @@
+question = input("what is your question?")
+if question == "Why is the sky blue?":
+    print("Because")
+if question == "What is the meaning of life?":
+    print("To be loved and to love others")
+if question == "Who is God?":
+    print("Something people made up")
+if question == "What is the funniest joke?":
+    print("Your face")
+else:
+    print("please ask something else.")
