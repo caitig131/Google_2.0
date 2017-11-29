ee6dc902a12848d09dd3c5fc39c67c4abd5cf735
caitig131 <31958666+caitig131@users.noreply.github.com>
2017-11-29 14:23:40 -0500

    Update Google2.0.py

--- Google2.0.py
+++ Google2.0.py
@@ -1,9 +1,22 @@
-question = input("what is your question?")
-if question == "Why is the sky blue?":
-    print("Because")
 if question == "What is the meaning of life?":
     print("To be loved and to love others")
 if question == "What is the funniest joke?":
     print("Your face")
+if question == "What is the best movie?":
+    print("The Bee Movie")
+if question == "Who put us on this Earth?":
+    print("The great beings of the planet Gab-Odon. They were bored one day and decided on creating life. We are a game that they made.")
+if question == "What is the best food?":
+    print("French fries hands down")
+if question == "Who are you?":
+    print("I am me. I am you. I am everyone who has ever lived; past, present, and future.")
+if question == "Why am I alive?":
+    print("Everyone on Earth has a purpose towards the greater power.")
+if question == "Who is the best President?":
+    print("There is none.")
+if question == "Who is the King of France?":
+    print("Taneil Knowles")
+if question == "Where is Tupac?":
+    print("Havana,Cuba.")
 else:
-    print("please ask something else.")
+    print("I do not have the answer. You have broken me. I am defeated.")
