075e03bf5ad018a598521028405aef8e392f4466
caitig131 <31958666+caitig131@users.noreply.github.com>
2018-01-12 08:25:21 -0500

    Update Google2.0.py

--- Google2.0.py
+++ Google2.0.py
@@ -1,22 +1,25 @@
-if question == "What is the meaning of life?":
+question = input("what is your question?")
+if question == "Why is the sky blue?":
+    print("Because")
+elif question == "What is the meaning of life?":
     print("To be loved and to love others")
-if question == "What is the funniest joke?":
+elif question == "What is the funniest joke?":
     print("Your face")
-if question == "What is the best movie?":
+elif question == "What is the best movie?":
     print("The Bee Movie")
-if question == "Who put us on this Earth?":
+elif question == "Who put us on this Earth?":
     print("The great beings of the planet Gab-Odon. They were bored one day and decided on creating life. We are a game that they made.")
-if question == "What is the best food?":
+elif question == "What is the best food?":
     print("French fries hands down")
-if question == "Who are you?":
+elif question == "Who are you?":
     print("I am me. I am you. I am everyone who has ever lived; past, present, and future.")
-if question == "Why am I alive?":
+elif question == "Why am I alive?":
     print("Everyone on Earth has a purpose towards the greater power.")
-if question == "Who is the best President?":
+elif question == "Who is the best President?":
     print("There is none.")
-if question == "Who is the King of France?":
+elif question == "Who is the King of France?":
     print("Taneil Knowles")
-if question == "Where is Tupac?":
+elif question == "Where is Tupac?":
     print("Havana,Cuba.")
 else:
     print("I do not have the answer. You have broken me. I am defeated.")
