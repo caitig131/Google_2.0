e572d65f14fad02a34a3c6d744877d8578fe8970
caitig131 <31958666+caitig131@users.noreply.github.com>
2018-05-18 14:56:51 -0400

    new classwork

--- Google 2.0 new.py
+++ Google 2.0 new.py
@@ -3,12 +3,34 @@ import pygame_textinput
 import pygame
 pygame.init()
 
-# Create TextInput-object
-textinput = pygame_textinput.TextInput()
+
 
 screen = pygame.display.set_mode((1000, 200))
 clock = pygame.time.Clock()
+
+class Quest():
+    question = "ttt"
+    qalist = [("Why is the sky blue?","Because"),("What is the meaning of life?","To be loved and to love others")]
+    def __init__(self, screen):
+        pass
+        # Create TextInput-object
+        #self.textinput = pygame_textinput.TextInput()
+        #self.screen= screen
+
+    def answer(self, question):
+        self.question = question
+        print(self.question)
+       # self.screen = screen
+        #events = pygame.event.get()
+        # Feed it with events every frame
+        #self.textinput.update(events)
+        #self.question = self.textinput.get_text()
+        # Blit its surface onto the screen
+        #self.screen.blit(self.textinput.get_surface(), (10, 10))
 question = ""
+quest = Quest(screen)
+textinput = pygame_textinput.TextInput()
+
 while True:
     screen.fill((225, 225, 225))
 
@@ -16,6 +38,11 @@ while True:
     for event in events:
         if event.type == pygame.QUIT:
             exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+            quest.answer(question)
+
+
+    #quest.ask()
 
     # Feed it with events every frame
     textinput.update(events)
